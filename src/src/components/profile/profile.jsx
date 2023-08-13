fb2c27ffd1661d5c0b0473a33038027fdec4cef0
saeedmaghdam <smasafat@gmail.com>
2023-08-13 20:02:03 +0200

    added profile page

--- src/src/components/profile/profile.jsx
+++ src/src/components/profile/profile.jsx
@@ -1,3 +1,54 @@
-const Profile = () => <h1>Profile</h1>
+const Profile = () => {
+  return (
+    <div className="ui grid">
+      <div className="row">
+        <div className="fifteen wide column">
+          <div className="ui breadcrumb">
+            <div className="active section">Profile</div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="sixteen wide column">
+          <form className="ui form">
+            <div className="field">
+              <label>Username</label>
+              <input placeholder="Username" disabled />
+            </div>
+            <div className="field">
+              <label>Email</label>
+              <input placeholder="Email" disabled />
+            </div>
+            <div className="field">
+              <label>Password</label>
+              <input placeholder="Password" />
+            </div>
+            <div className="field">
+              <label>Confirm Password</label>
+              <input placeholder="Confirm Password" />
+            </div>
+            <button className="ui button" type="submit">
+              Save
+            </button>
+          </form>
+          <div class="ui divider"></div>
+          <form className="ui form">
+            <div className="field">
+              <label>Name</label>
+              <input placeholder="Name" />
+            </div>
+            <div className="field">
+              <label>Family</label>
+              <input placeholder="Family" />
+            </div>
+            <button className="ui button" type="submit">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
