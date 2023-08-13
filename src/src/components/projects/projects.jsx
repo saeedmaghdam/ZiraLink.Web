f6a44cd5173d9acbd17975725427497447fad46f
saeedmaghdam <smasafat@gmail.com>
2023-08-13 15:54:45 +0200

    added grid system + breadcrumb to projects

--- src/src/components/projects/projects.jsx
+++ src/src/components/projects/projects.jsx
@@ -2,148 +2,183 @@ import styles from "./projects.module.css";
 
 const Projects = () => {
   return (
-    <>
-      <button class="ui active blue button">
-        <i class="icon cube"></i>
-        New Project
-      </button>
-      <table class="ui single line table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Domain</th>
-            <th>Username</th>
-            <th>State</th>
-            <th>Update Date</th>
-            <th>Full Name</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Test React App</td>
-            <td>
-              <span className="bold">test</span>
-              <span className={styles.gray}>.zira.aghdam.nl</span>
-            </td>
-            <td>saeedmaghdam</td>
-            <td>Saeed Aghdam</td>
-            <td>
-              <span className={`ui green message ${styles.state}`}>Active</span>
-            </td>
-            <td>August 13, 2023</td>
-            <td>
-              <button class="circular ui icon button">
-                <i class="icon edit"></i>
-              </button>
-              <button class="circular ui icon button">
-                <i class="icon file"></i>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>IIS Test Project</td>
-            <td>
-              <span className="bold">iis</span>
-              <span className={styles.gray}>.zira.aghdam.nl</span>
-            </td>
-            <td>saeedmaghdam</td>
-            <td>Saeed Aghdam</td>
-            <td>
-              <span className={`ui green message ${styles.state}`}>Active</span>
-            </td>
-            <td>August 13, 2023</td>
-            <td>
-              <button class="circular ui icon button">
-                <i class="icon edit"></i>
-              </button>
-              <button class="circular ui icon button">
-                <i class="icon file"></i>
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>antaeus.md.api</td>
-            <td>
-              <span className="bold">antaeus-md-api</span>
-              <span className={styles.gray}>.zira.aghdam.nl</span>
-            </td>
-            <td>saeedmaghdam</td>
-            <td>Saeed Aghdam</td>
-            <td>
-              <span className={`ui red message ${styles.state}`}>Inactive</span>
-            </td>
-            <td>August 13, 2023</td>
-            <td>
-            <button class="circular ui icon button">
-                <i class="icon edit"></i>
-              </button>
-              <button class="circular ui icon button">
-                <i class="icon file"></i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div
-        aria-label="Pagination Navigation"
-        role="navigation"
-        class="ui pagination menu"
-      >
-        <a
-          aria-current="false"
-          aria-disabled="false"
-          tabindex="0"
-          value="1"
-          aria-label="Previous item"
-          type="prevItem"
-          class="item"
-        >
-          ⟨
-        </a>
-        <a
-          aria-current="true"
-          aria-disabled="false"
-          tabindex="0"
-          value="1"
-          type="pageItem"
-          class="active item"
-        >
-          1
-        </a>
-        <a
-          aria-current="false"
-          aria-disabled="false"
-          tabindex="0"
-          value="2"
-          type="pageItem"
-          class="item"
-        >
-          2
-        </a>
-        <a
-          aria-current="false"
-          aria-disabled="false"
-          tabindex="0"
-          value="3"
-          type="pageItem"
-          class="item"
-        >
-          3
-        </a>
-        <a
-          aria-current="false"
-          aria-disabled="false"
-          tabindex="0"
-          value="2"
-          aria-label="Next item"
-          type="nextItem"
-          class="item"
-        >
-          ⟩
-        </a>
+    <div className="ui grid">
+      <div className="row">
+        <div className={`sixteen wide column ${styles.breadcrumbContainer}`}>
+          <div class="ui breadcrumb">
+            <div class="active section">Projects</div>
+          </div>
+        </div>
       </div>
-    </>
+      <div className="row">
+        <div className="four wide column">
+          <button class="ui active blue button">
+            <i class="icon cube"></i>
+            New Project
+          </button>
+        </div>
+        <div className="twelve wide column right aligned">
+          <div class="ui icon input">
+            <input
+              type="text"
+              placeholder="Search..."
+              data-listener-added_f3844f1b="true"
+            />
+            <i class="circular search link icon"></i>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="sixteen wide column">
+          <table class="ui single line table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Domain</th>
+                <th>Username</th>
+                <th>State</th>
+                <th>Update Date</th>
+                <th>Full Name</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Test React App</td>
+                <td>
+                  <span className="bold">test</span>
+                  <span className={styles.gray}>.zira.aghdam.nl</span>
+                </td>
+                <td>saeedmaghdam</td>
+                <td>Saeed Aghdam</td>
+                <td>
+                  <span className={`ui green message ${styles.state}`}>
+                    Active
+                  </span>
+                </td>
+                <td>August 13, 2023</td>
+                <td>
+                  <button class="circular ui icon button">
+                    <i class="icon edit"></i>
+                  </button>
+                  <button class="circular ui icon button">
+                    <i class="icon file"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>IIS Test Project</td>
+                <td>
+                  <span className="bold">iis</span>
+                  <span className={styles.gray}>.zira.aghdam.nl</span>
+                </td>
+                <td>saeedmaghdam</td>
+                <td>Saeed Aghdam</td>
+                <td>
+                  <span className={`ui green message ${styles.state}`}>
+                    Active
+                  </span>
+                </td>
+                <td>August 13, 2023</td>
+                <td>
+                  <button class="circular ui icon button">
+                    <i class="icon edit"></i>
+                  </button>
+                  <button class="circular ui icon button">
+                    <i class="icon file"></i>
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>antaeus.md.api</td>
+                <td>
+                  <span className="bold">antaeus-md-api</span>
+                  <span className={styles.gray}>.zira.aghdam.nl</span>
+                </td>
+                <td>saeedmaghdam</td>
+                <td>Saeed Aghdam</td>
+                <td>
+                  <span className={`ui red message ${styles.state}`}>
+                    Inactive
+                  </span>
+                </td>
+                <td>August 13, 2023</td>
+                <td>
+                  <button class="circular ui icon button">
+                    <i class="icon edit"></i>
+                  </button>
+                  <button class="circular ui icon button">
+                    <i class="icon file"></i>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="row">
+        <div className="sixteen wide column">
+          <div
+            aria-label="Pagination Navigation"
+            role="navigation"
+            class="ui pagination menu"
+          >
+            <a
+              aria-current="false"
+              aria-disabled="false"
+              tabindex="0"
+              value="1"
+              aria-label="Previous item"
+              type="prevItem"
+              class="item"
+            >
+              ⟨
+            </a>
+            <a
+              aria-current="true"
+              aria-disabled="false"
+              tabindex="0"
+              value="1"
+              type="pageItem"
+              class="active item"
+            >
+              1
+            </a>
+            <a
+              aria-current="false"
+              aria-disabled="false"
+              tabindex="0"
+              value="2"
+              type="pageItem"
+              class="item"
+            >
+              2
+            </a>
+            <a
+              aria-current="false"
+              aria-disabled="false"
+              tabindex="0"
+              value="3"
+              type="pageItem"
+              class="item"
+            >
+              3
+            </a>
+            <a
+              aria-current="false"
+              aria-disabled="false"
+              tabindex="0"
+              value="2"
+              aria-label="Next item"
+              type="nextItem"
+              class="item"
+            >
+              ⟩
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
