d0e5558164c42916c1e20a9db7d935bfb7d71af5
saeedmaghdam <smasafat@gmail.com>
2023-08-22 02:22:51 +0200

    added macos client download link

--- src/src/components/downloads/downloads.jsx
+++ src/src/components/downloads/downloads.jsx
@@ -17,6 +17,9 @@ const Downloads = () => (
         <a href="/static/linux-x64.zip" target="_black" className={styles.downloadLink}>
           <i class="cloud download icon"></i>Linux x64
         </a>
+        <a href="/static/osx-x64.zip" target="_black" className={styles.downloadLink}>
+          <i class="cloud download icon"></i>macOS x64
+        </a>
       </div>
     </div>
   </div>
