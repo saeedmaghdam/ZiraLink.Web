7bf3247fbbd15971a88a11bbdfee8ec3f249921f
saeedmaghdam <smasafat@gmail.com>
2023-10-13 19:12:42 +0200

    Revert "temp: updated config"
    This reverts commit 4511ef4b8230929cead45b25908bf48bc1954202.

--- src/src/config.js
+++ src/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   BASE_URL: 'https://ziralink-api.kub.local',
   IDS_URL: 'https://ziralink-api.kub.local',
-  DEFAULT_DOMAIN: '.kub.local'
+  DEFAULT_DOMAIN: .kub.local
 };
 
 export default config;
